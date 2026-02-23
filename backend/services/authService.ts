5490c77ead77bca3ec2ef5184f84bcffde118bcc
ice Thawatchai <thipunyatpny@gmail.com>
2026-02-23 08:49:26 +0700

    DeployError6

--- backend/services/authService.ts
+++ backend/services/authService.ts
@@ -2,7 +2,6 @@ import { prisma } from "@/lib/db";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
-// import { User } from "@/app/cms/lib/types";
 import { User as PrismaUser } from "@prisma/client";
 
 const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
@@ -21,7 +20,7 @@ export const authService = {
     return await prisma.user.findUnique({ where: { email } });
   },
 
-  generateSessionToken(user: Pick<PrismaUser, 'id' | 'email'>) {
+  generateSessionToken(user: PrismaUser) {
     return jwt.sign(
       { userId: user.id, email: user.email },
       JWT_SECRET,
